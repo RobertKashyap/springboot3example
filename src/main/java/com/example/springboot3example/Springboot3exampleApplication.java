9f0da924c280bb1a26786c4806f3cd68c950143f
Robert Kashyap <orionalchemist@gmail.com>
2023-11-13 23:23:02 +0530

    initialzed spring boot 3 usage demonstration repo

---
+++ src/main/java/com/example/springboot3example/Springboot3exampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot3example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3exampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot3exampleApplication.class, args);
+	}
+
+}
