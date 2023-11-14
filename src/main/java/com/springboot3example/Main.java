203440dd1866f7aaa9e96cb8264c84075f5013bf
Robert Kashyap <orionalchemist@gmail.com>
2023-11-14 20:44:32 +0530

    created get mapping api endpoint

--- src/main/java/com/springboot3example/Main.java
+++ src/main/java/com/springboot3example/Main.java
@@ -2,11 +2,19 @@ package com.springboot3example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         // System.out.println("Hello World!");
-        SpringApplication.run(Main.class, args);//starts a basic tomcat server
+        SpringApplication.run(Main.class, args);// starts a basic tomcat server
+    }
+
+    @GetMapping("/greet")
+    public String greet() {
+        return "Hello";
     }
 }
