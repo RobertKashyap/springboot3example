0c52264f8c2409bc4b1c3bfecdd6adbfdebe4a58
Robert Kashyap <orionalchemist@gmail.com>
2023-11-14 12:46:56 +0530

    initialize springboot proj to enable tomcat server

--- src/main/java/com/springboot3example/Main.java
+++ src/main/java/com/springboot3example/Main.java
@@ -1,7 +1,12 @@
 package com.springboot3example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // System.out.println("Hello World!");
+        SpringApplication.run(Main.class, args);//starts a basic tomcat server
     }
 }
