a594a8c78d060c32e45e9ed32c23614bd4f7a2b8
Robert Kashyap <orionalchemist@gmail.com>
2023-11-14 21:29:18 +0530

    explored nested JSON support of Jackson

--- src/main/java/com/springboot3example/Main.java
+++ src/main/java/com/springboot3example/Main.java
@@ -1,5 +1,7 @@
 package com.springboot3example;
 
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,10 +17,21 @@ public class Main {
 
     @GetMapping("/greet")
     public GreetResponse greet() {
-        return new GreetResponse("Hello");
+        GreetResponse response = new GreetResponse(
+                "Hello",
+                new Person("Robert", 24, 30_000.05),
+                List.of("java", "ASM", "Bfck"));
+        return response;
     }
 
-    // POJO blueprint to return JSON data via rest annotation (via jackson library, json parser for java)
-    record GreetResponse(String greet) {
+    // POJO blueprint to return JSON data via rest annotation (via jackson library,
+    // json parser for java)
+    record GreetResponse(
+            String greet,
+            Person person,
+            List<String> favProgLang) {
+    }
+
+    record Person(String name, int age, double savings) {
     }
 }
