eba8d724ffd753a001930dc00b2e4ad8002cad73
Robert Kashyap <orionalchemist@gmail.com>
2023-11-15 20:40:19 +0530

    bare minimum jpa config for table gen in db

--- src/main/java/com/springboot3example/Customer.java
+++ src/main/java/com/springboot3example/Customer.java
@@ -2,7 +2,17 @@ package com.springboot3example;
 
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+
+@Entity
 public class Customer {
+    @Id
+    @SequenceGenerator(name = "customer_id_sequence", sequenceName = "customer_id_sequence")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "customer_id_sequence")
     private Integer id;
     private String name;
     private String email;
